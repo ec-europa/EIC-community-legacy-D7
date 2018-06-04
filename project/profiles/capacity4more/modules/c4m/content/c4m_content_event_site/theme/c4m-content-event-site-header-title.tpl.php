4e827891570d31fc298219e5233ea4a90bff6787
Vlas Tudor <tudor.vlas@amplexor.com>
2018-06-04 15:55:18 +0300

    EASME-142: Shortcut for going from Draft to Published state in the front end of an event site

--- project/profiles/capacity4more/modules/c4m/content/c4m_content_event_site/theme/c4m-content-event-site-header-title.tpl.php
+++ project/profiles/capacity4more/modules/c4m/content/c4m_content_event_site/theme/c4m-content-event-site-header-title.tpl.php
@@ -9,7 +9,7 @@
   <?php if ($type): ?>
     <span class="top-buffer indication label label-default project-type"><?php print $type; ?></span>
   <?php endif; ?>
-  
+
   <?php if ($status): ?>
     <span class="top-buffer indication label project-status <?php print (!empty($label_type)) ? $label_type : ''; ?>"><?php print $status; ?></span>
   <?php endif; ?>
@@ -20,8 +20,16 @@
     <?php print $title; ?>
   </h1>
 <?php endif; ?>
+
 <?php if ($tagline): ?>
   <h2 class="project-subtitle">
     <?php print $tagline; ?>
   </h2>
+<?php endif; ?>
+
+
+<?php if ($publish_link): ?>
+    <div class="publish-link">
+      <span> <?php print $publish_link; ?></span>
+    </div>
 <?php endif; ?>
