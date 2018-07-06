f348725a98a505278483f2b3a576ef4150863ade
Vlas Tudor <tudor.vlas@amplexor.com>
2018-07-06 14:51:55 +0300

    EASME-186: Fixed hamburger menu for organisations and event sites

--- project/profiles/capacity4more/themes/c4m/shake/templates/page.tpl.php
+++ project/profiles/capacity4more/themes/c4m/shake/templates/page.tpl.php
@@ -80,49 +80,62 @@
 
 <header id="page-header" role="banner">
 
-  <!--  USER BAR -->
+    <!--  USER BAR -->
   <?php if (!empty($page['user_bar'])): ?>
-    <section id="user-bar">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-12">
-            <?php print render($page['user_bar']); ?>
+      <section id="user-bar">
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-12 col-md-12">
+                    <?php print render($page['user_bar']); ?>
+                  </div>
+              </div>
           </div>
-        </div>
-      </div>
-    </section>
+      </section>
   <?php endif; ?>
 
-  <div id="header-ec-wrapper">
-    <?php if ($image_banner_background): ?>
-        <div class="background-banner">
-          <?php print $image_banner_background; ?>
-        </div>
-    <?php endif; ?>
-      <div class="container">
-      <div class="row">
-        <div class="col-xs-9 col-sm-8 col-md-9 header-logo">
-          <?php if ($logo): ?>
-            <a class="logo pull-left" href="<?php print $front_page; ?>"
-               title="<?php print t('Home'); ?>">
-              <img src="<?php print $logo; ?>" alt="<?php print t('capacity4dev logo'); ?>" title="<?php print t('capacity4dev logo'); ?>"/>
-            </a>
-          <?php endif; ?>
+    <!--  TOP MENU -->
+  <?php if (!empty($page['top_menu'])): ?>
+      <section id="top-menu">
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-12 col-md-12 text-right">
+                    <?php print render($page['top_menu']); ?>
+                  </div>
+              </div>
+          </div>
+      </section>
+  <?php endif; ?>
+
+    <div id="header-ec-wrapper">
+      <?php if ($image_banner_background): ?>
+          <div class="background-banner">
+            <?php print $image_banner_background; ?>
+          </div>
+      <?php endif; ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-9 col-sm-8 col-md-9 header-logo">
+                  <?php if ($logo): ?>
+                      <a class="logo pull-left" href="<?php print $front_page; ?>"
+                         title="<?php print t('Home'); ?>">
+                          <img src="<?php print $logo; ?>" alt="<?php print t('capacity4dev logo'); ?>" title="<?php print t('capacity4dev logo'); ?>"/>
+                      </a>
+                  <?php endif; ?>
             <div class="site-name-slogan">
               <h1><?php print (!empty($site_name)) ? $site_name : ''; ?></h1>
               <h2><?php print (!empty($site_slogan)) ? $site_slogan : ''; ?></h2>
             </div>
-        </div>
+                </div>
 
-        <div class="col-xs-3 col-sm-4 col-md-3">
+                <div class="col-xs-3 col-sm-4 col-md-3">
 
-          <div class="header-actions">
+                    <div class="header-actions">
 
             <span class="header-actions--wrapper">
               <span class="header-actions--search fa fa-search"
                     data-toggle="collapse" data-target="#search"></span>
             </span>
-            <span class="header-actions--wrapper">
+                        <span class="header-actions--wrapper">
               <div class="header-actions--navigation js-navigationButton"
                    data-effect="animation--push">
                 <span class="icon-bar"></span>
@@ -130,200 +143,200 @@
                 <span class="icon-bar"></span>
               </div>
             </span>
-          </div>
+                    </div>
 
-        </div>
+                </div>
 
 
-        <div id="search"
-             class="col-xs-4 col-sm-4 col-md-3 header-search collapse">
-          <!-- SEARCH-->
-          <?php print render($search_form); ?>
-        </div>
-      </div>
-
-        <?php if ($image_banner): ?>
-            <div class="row">
-                <div class="col-sm-12 col-md-12">
-                  <?php print $image_banner; ?>
+                <div id="search"
+                     class="col-xs-4 col-sm-4 col-md-3 header-search collapse">
+                    <!-- SEARCH-->
+                  <?php print render($search_form); ?>
                 </div>
             </div>
-        <?php endif; ?>
-      </div>
-  </div><!-- /banner-ec -->
+
+          <?php if ($image_banner): ?>
+              <div class="row">
+                  <div class="col-sm-12 col-md-12">
+                    <?php print $image_banner; ?>
+                  </div>
+              </div>
+          <?php endif; ?>
+        </div>
+    </div><!-- /banner-ec -->
 
   <div id="ecl-page-header">
-    <div class="container">
-      <div class="row">
+          <div class="container">
+              <div class="row">
         <div class="col-sm-12 col-md-12 header-breadcrumb">
           <?php if (!empty($breadcrumb)): print $breadcrumb;
           endif; ?>
-        </div>
-      </div>
-    </div>
+                  </div>
+              </div>
+          </div>
   </div>
 
   <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-    <div class="navigation--primary">
-      <nav class="container navbar" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-          <button type="button" class="navbar-toggle" data-toggle="collapse"
-                  data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-        </div>
+      <div class="navigation--primary">
+          <nav class="container navbar" role="navigation">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+                  <button type="button" class="navbar-toggle" data-toggle="collapse"
+                          data-target=".navbar-collapse">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                  </button>
+              </div>
 
-        <div class="navbar-collapse collapse">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </div>
-      </nav>
-    </div>
+              <div class="navbar-collapse collapse">
+                <?php if (!empty($primary_nav)): ?>
+                  <?php print render($primary_nav); ?>
+                <?php endif; ?>
+                <?php if (!empty($page['navigation'])): ?>
+                  <?php print render($page['navigation']); ?>
+                <?php endif; ?>
+              </div>
+          </nav>
+      </div>
   <?php endif; ?>
 </header>
 
 <?php if (!empty($page['content_top'])): ?>
-  <div id="content-top-wrapper">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-12 content-top">
-          <?php print render($page['content_top']); ?>
+    <div id="content-top-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 content-top">
+                  <?php print render($page['content_top']); ?>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 <?php endif; ?>
 <?php if (!empty($page['content_above'])): ?>
-  <div id="content-above-wrapper">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-12 content-above">
-          <?php print render($page['content_above']); ?>
+    <div id="content-above-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 content-above">
+                  <?php print render($page['content_above']); ?>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 <?php endif; ?>
 
 <div id="main-wrapper">
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-12 col-md-12">
-        <div class="trigger trigger--groupInfo js-navigationButton"
-             data-effect="animation--slideOn">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12">
+                <div class="trigger trigger--groupInfo js-navigationButton"
+                     data-effect="animation--slideOn">
           <span class="trigger-label circle circle--green">
             <span class="fa fa-info"></span>
           </span>
-          <span class="trigger-text">Group info</span>
-          <i class="pull-right fa fa-chevron-right"></i>
+                    <span class="trigger-text">Group info</span>
+                    <i class="pull-right fa fa-chevron-right"></i>
+                </div>
+
+              <?php if (!empty($page['sidebar_first']) || !empty($page['sidebar_first_top'])): ?>
+                  <aside class="col-sm-4 offCanvasNavigation--left"
+                         role="complementary">
+                    <?php print render($page['sidebar_first_top']); ?>
+                    <?php print render($page['sidebar_first']); ?>
+                  </aside>  <!-- /#sidebar-first -->
+              <?php endif; ?>
+
+                <section<?php print $content_column_class_kapablo; ?>>
+                  <?php if (!empty($page['highlighted'])): ?>
+                      <div class="highlighted well"><?php print render(
+                          $page['highlighted']
+                        ); ?></div>
+                  <?php endif; ?>
+                    <a id="main-content"></a>
+                  <?php print render($title_prefix); ?>
+                  <?php if (!empty($title)): ?>
+                      <h1 class="page-header"><?php print $title; ?></h1>
+                  <?php endif; ?>
+                  <?php print render($title_suffix); ?>
+                  <?php print $messages; ?>
+                  <?php if (!empty($tabs)): ?>
+                    <?php print render($tabs); ?>
+                  <?php endif; ?>
+                  <?php if (!empty($page['help'])): ?>
+                    <?php print render($page['help']); ?>
+                  <?php endif; ?>
+                  <?php if (!empty($action_links)): ?>
+                      <ul class="action-links"><?php print render($action_links); ?></ul>
+                  <?php endif; ?>
+
+                  <?php if (!empty($page['sidebar_first']) && !empty($offcanvas_trigger_label_left)): ?>
+                      <div class="trigger js-navigationButton clearfix"
+                           data-effect="animation--slideOn">
+                          <i class="pull-left fa fa-chevron-left"></i>
+                          <span
+                                  class="trigger-text pull-right"><?php print $offcanvas_trigger_label_left['label']; ?></span>
+                        <?php if ($offcanvas_trigger_label_left['icon']): ?>
+                            <span class="trigger-label circle circle--white pull-right">
+                <span
+                        class="fa fa-<?php print $offcanvas_trigger_label_left['icon']; ?>"></span>
+              </span>
+                        <?php endif; ?>
+                      </div>
+                  <?php endif; ?>
+
+                  <?php if (!empty($page['sidebar_second']) && !empty($offcanvas_trigger_label_right)): ?>
+                      <div class="trigger js-navigationButton clearfix"
+                           data-effect="animation--slideOn">
+                        <?php if ($offcanvas_trigger_label_left['icon']): ?>
+                            <span class="trigger-label pull-left circle circle--white">
+                <span
+                        class="fa fa-<?php print $offcanvas_trigger_label_right['icon']; ?>"></span>
+              </span>
+                        <?php endif; ?>
+                          <span
+                                  class="trigger-text pull-left"><?php print $offcanvas_trigger_label_right['label']; ?></span>
+                          <i class="pull-right fa fa-chevron-right"></i>
+                      </div>
+                  <?php endif; ?>
+                  <?php print render($page['content']); ?>
+                </section>
+
+              <?php if (!empty($page['sidebar_second'])): ?>
+                  <aside class="col-sm-4 offCanvasNavigation--right"
+                         role="complementary">
+                    <?php print render($page['sidebar_second']); ?>
+                  </aside>  <!-- /#sidebar-second -->
+              <?php endif; ?>
+            </div>
         </div>
-
-        <?php if (!empty($page['sidebar_first']) || !empty($page['sidebar_first_top'])): ?>
-          <aside class="col-sm-4 offCanvasNavigation--left"
-                 role="complementary">
-            <?php print render($page['sidebar_first_top']); ?>
-            <?php print render($page['sidebar_first']); ?>
-          </aside>  <!-- /#sidebar-first -->
-        <?php endif; ?>
-
-        <section<?php print $content_column_class_kapablo; ?>>
-          <?php if (!empty($page['highlighted'])): ?>
-            <div class="highlighted well"><?php print render(
-                $page['highlighted']
-              ); ?></div>
-          <?php endif; ?>
-          <a id="main-content"></a>
-          <?php print render($title_prefix); ?>
-          <?php if (!empty($title)): ?>
-            <h1 class="page-header"><?php print $title; ?></h1>
-          <?php endif; ?>
-          <?php print render($title_suffix); ?>
-          <?php print $messages; ?>
-          <?php if (!empty($tabs)): ?>
-            <?php print render($tabs); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['help'])): ?>
-            <?php print render($page['help']); ?>
-          <?php endif; ?>
-          <?php if (!empty($action_links)): ?>
-            <ul class="action-links"><?php print render($action_links); ?></ul>
-          <?php endif; ?>
-
-          <?php if (!empty($page['sidebar_first']) && !empty($offcanvas_trigger_label_left)): ?>
-            <div class="trigger js-navigationButton clearfix"
-                 data-effect="animation--slideOn">
-              <i class="pull-left fa fa-chevron-left"></i>
-              <span
-                class="trigger-text pull-right"><?php print $offcanvas_trigger_label_left['label']; ?></span>
-              <?php if ($offcanvas_trigger_label_left['icon']): ?>
-                <span class="trigger-label circle circle--white pull-right">
-                <span
-                  class="fa fa-<?php print $offcanvas_trigger_label_left['icon']; ?>"></span>
-              </span>
-              <?php endif; ?>
-            </div>
-          <?php endif; ?>
-
-          <?php if (!empty($page['sidebar_second']) && !empty($offcanvas_trigger_label_right)): ?>
-            <div class="trigger js-navigationButton clearfix"
-                 data-effect="animation--slideOn">
-              <?php if ($offcanvas_trigger_label_left['icon']): ?>
-                <span class="trigger-label pull-left circle circle--white">
-                <span
-                  class="fa fa-<?php print $offcanvas_trigger_label_right['icon']; ?>"></span>
-              </span>
-              <?php endif; ?>
-              <span
-                class="trigger-text pull-left"><?php print $offcanvas_trigger_label_right['label']; ?></span>
-              <i class="pull-right fa fa-chevron-right"></i>
-            </div>
-          <?php endif; ?>
-          <?php print render($page['content']); ?>
-        </section>
-
-        <?php if (!empty($page['sidebar_second'])): ?>
-          <aside class="col-sm-4 offCanvasNavigation--right"
-                 role="complementary">
-            <?php print render($page['sidebar_second']); ?>
-          </aside>  <!-- /#sidebar-second -->
-        <?php endif; ?>
-      </div>
     </div>
-  </div>
 
     <footer class="ecl-footer">
-      <?php if (!empty($page['footer_top'])): ?>
+  <?php if (!empty($page['footer_top'])): ?>
       <div id="footer-top-wrapper">
-        <div class="container">
-          <div class="row">
-            <section class="col-md-12">
-              <?php print render($page['footer_top']); ?>
-            </section>
+          <div class="container">
+              <div class="row">
+                  <section class="col-md-12">
+                    <?php print render($page['footer_top']); ?>
+                  </section>
+              </div>
           </div>
-        </div>
       </div>
-      <?php endif; ?>
+  <?php endif; ?>
 
 
-      <?php if (!empty($page['footer_bottom'])): ?>
+  <?php if (!empty($page['footer_bottom'])): ?>
       <div id="footer-bottom-wrapper">
-        <div class="container">
-          <div class="row">
-            <section class="col-md-12">
-              <?php print render($page['footer_bottom']); ?>
-            </section>
+          <div class="container">
+              <div class="row">
+                  <section class="col-md-12">
+                    <?php print render($page['footer_bottom']); ?>
+                  </section>
+              </div>
           </div>
-        </div>
       </div>
-      <?php endif; ?>
+  <?php endif; ?>
     </footer>
 </div>
