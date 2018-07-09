e697d023a9429a00c5ae76774304920e64ef8862
Vlas Tudor <tudor.vlas@amplexor.com>
2018-07-09 16:54:01 +0300

    EASME-70: Group nodes: Highlight in which section you are (News, Wiki, ...)

--- project/profiles/capacity4more/themes/c4m/shake/template.php
+++ project/profiles/capacity4more/themes/c4m/shake/template.php
@@ -15,3 +15,42 @@ function shake_preprocess_page(&$variables) {
     }
   }
 }
+
+/**
+ * Implements template_menu_link().
+ */
+function shake_menu_link(&$variables) {
+  $element  = ($variables['element'])? $variables['element'] : '';
+  $menuName = isset($variables['element']["#original_link"]["menu_name"])?
+    $variables['element']["#original_link"]["menu_name"]: '';
+
+  if($menuName == 'main-menu' && !empty($element)) {
+
+    $active_path = FALSE;
+    if ($node = menu_get_object()) {
+      switch ($node->type) {
+        case 'article':
+          $active_path = 'articles';
+          break;
+
+        case 'event_site':
+          $active_path = 'calendar';
+          break;
+
+        case 'topic':
+          $active_path = 'topics';
+          break;
+      }
+    }
+    if (arg(0) == 'user') {
+      $active_path = 'people';
+    }
+
+    if ($active_path && $element['#href'] == $active_path) {
+      $element["#attributes"]['class'][] = 'active';
+    }
+
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . "</li>";
+}
