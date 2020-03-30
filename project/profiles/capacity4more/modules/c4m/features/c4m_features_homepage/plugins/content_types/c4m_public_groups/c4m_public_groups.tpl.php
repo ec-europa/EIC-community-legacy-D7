b474f12620382acfcfc26f8b8038c4377feb31e0
LOENDERS Siggy <siggy.loenders@ext.ec.europa.eu>
2020-03-30 16:41:54 +0200

    EIC/feature/EICNET-47: added custom plugin to create block for groups , for now it shows all the groups, initial testing.

---
+++ project/profiles/capacity4more/modules/c4m/features/c4m_features_homepage/plugins/content_types/c4m_public_groups/c4m_public_groups.tpl.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Prints out the list of suggested groups.
+ */
+?>
+
+<?php
+$classes = '';
+if ($display_more_button) {
+  $classes = ' pane--more';
+}
+?>
+
+<div class="sidebarblock suggested-groups<?php print $classes; ?>">
+  <h2 class="sidebarblock__title"><?php print t('Public Groups') ?></h2>
+  <?php print $groups ?>
+  <?php if ($display_more_button): ?>
+  <div class="sidebarblock__viewmore">
+    <a href="<?php print $link; ?>"><?php print t('See more') ?> <i class="fa fa-chevron-right"></i></a>
+  </div>
+  <?php endif; ?>
+</div>
